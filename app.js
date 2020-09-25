d79e96fb8b43115282fa41e6f284df92d8b3230c
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-09-25 14:58:40 +0400

    B011 Rate-limiter, cookie-parser, loggers

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// const rateLimit = require('express-rate-limit');
+const rateLimit = require('express-rate-limit');
 const express = require('express');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const { requestLogger, errorLogger } = require('./middleware/logger');
 
+const { requestLogger, errorLogger } = require('./middleware/logger');
 const { PORT, DATABASE_ADDRESS } = require('./configs/config');
 const routes = require('./routes/routes');
 const errorHandler = require('./middleware/error-handler');
@@ -17,35 +17,24 @@ mongoose.connect(DATABASE_ADDRESS, {
   useUnifiedTopology: true,
 });
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 100,
-// });
-
-// app.use(limiter);
+app.use(limiter);
 app.use(bodyParser.json());
-// app.use(cookieParser());
-// app.use(requestLogger);
+app.use(cookieParser());
+app.use(requestLogger);
 // app.get(`${BASE_PATH}/crash-test`, () => {
 //   setTimeout(() => {
 //     throw new Error('Сервер сейчас упадёт');
 //   }, 0);
 // });
-
 app.use(routes);
-// app.use(errorLogger);
+app.use(errorLogger);
 app.use(celebValidateRequest);
 app.use(errorHandler);
-// app.use((err, req, res, next) => {
-//   const { statusCode = 500, message } = err;
-//   res.status(statusCode).send({
-//     message: statusCode === 500
-//       ? `На сервере произошла ошибка: ${message}`
-//       : message,
-//   });
-//   next();
-// });
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Сервер запущен, порт: ${PORT}.`);
