b9a46f694f5d4491cba96e73f9962b35697f2e03
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-09-30 19:00:37 +0400

    B035 Fixed a bad path for the previous

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const { errors } = require('./errorMessages');
+const { errors } = require('../configs/errorMessages');
 // const InvalidObjectIdError = require('../errors/InvalidObjectIdError');
 
 function joinErrorMessages(errorObject) {
