93eca2453dee7c566233816dbeefc12455498fbc
Vitaliy Tikhonov <vitaliy.a.tikhonov@yandex.ru>
2020-10-01 19:37:32 +0400

    B043 Moved the crash test up

--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,16 @@ const articles = require('./articles');
 const { BASE_PATH } = require('../configs/config');
 const NotFoundError = require('../errors/NotFoundError');
 
-router.use(`${BASE_PATH}signup`, signup);
-router.use(`${BASE_PATH}signin`, signin);
-router.use(auth);
-router.use(`${BASE_PATH}users`, users);
-router.use(`${BASE_PATH}articles`, articles);
 router.get(`${BASE_PATH}crash-test`, () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
   }, 0);
 });
+router.use(`${BASE_PATH}signup`, signup);
+router.use(`${BASE_PATH}signin`, signin);
+router.use(auth);
+router.use(`${BASE_PATH}users`, users);
+router.use(`${BASE_PATH}articles`, articles);
 router.use((req, res, next) => next(new NotFoundError()));
 
 module.exports = router;
